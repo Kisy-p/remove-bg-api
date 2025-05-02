edcb5d7fdee381cd5593d0e6d30f870881cb0f8a
Kisy-p <m.eluard@gmail.com>
2025-05-02 19:19:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
-from rembg import remove
+from rembg import remove, new_session
 import io
 
 app = FastAPI()
 
+# On utilise le modèle le plus léger : u2netp (~5 Mo, RAM-friendly)
+session = new_session("u2netp")
+
 @app.post("/remove-bg/")
 async def remove_bg(file: UploadFile = File(...)):
     image_bytes = await file.read()
-    result = remove(image_bytes)
-
-    return StreamingResponse(io.BytesIO(result), media_type="image/png")
+    output = remove(image_bytes, session=session)
+    return StreamingResponse(io.BytesIO(output), media_type="image/png")
