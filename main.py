de579ff3e92d13532d249eb0dbae0ebce1318a6e
Kisy-p <m.eluard@gmail.com>
2025-05-02 22:05:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,19 @@ import io
 
 app = FastAPI()
 
-try:
-    session = new_session("u2netp")  # modèle léger
-except Exception as e:
-    print(f"[ERREUR CRITIQUE] Chargement modèle : {e}")
-    raise
-
 @app.post("/remove-bg/")
 async def remove_bg(file: UploadFile = File(...)):
     try:
-        print("[INFO] Début du traitement")
         image_bytes = await file.read()
 
         if not image_bytes:
             raise HTTPException(status_code=400, detail="Fichier vide")
 
-        print(f"[INFO] Taille : {len(image_bytes)}")
+        session = new_session("u2netp")  # modèle léger
+        output = remove(image_bytes, session=session)
 
-        output_bytes = remove(image_bytes, session=session)
-
-        print("[INFO] Traitement réussi")
-        return StreamingResponse(io.BytesIO(output_bytes), media_type="image/png")
+        return StreamingResponse(io.BytesIO(output), media_type="image/png")
 
     except Exception as e:
-        print(f"[ERREUR] {e}")
+        print(f"[ERREUR SERVEUR] {e}")
         raise HTTPException(status_code=500, detail="Erreur serveur")
