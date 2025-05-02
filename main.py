a8531ff9d6e780a8c7382faea6cfcb7fe3df8f7b
Kisy-p <m.eluard@gmail.com>
2025-05-02 18:22:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ app = FastAPI()
 
 @app.post("/remove-bg/")
 async def remove_bg(file: UploadFile = File(...)):
-    # Lire les données brutes de l'image
     image_bytes = await file.read()
-    input_image = Image.open(io.BytesIO(image_bytes))
-
-    # Supprimer l’arrière-plan
     output_bytes = remove(image_bytes)
 
-    # Retourner l’image traitée directement
-    return StreamingResponse(io.BytesIO(output_bytes), media_type="image/png")
+    output_stream = io.BytesIO(output_bytes)
+    output_stream.seek(0)  # Important !
+
+    return StreamingResponse(output_stream, media_type="image/png")
