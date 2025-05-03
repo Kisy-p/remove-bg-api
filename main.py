7d1cc6c2422769a0ee81e8dcf09052c7a240234c
Kisy-p <m.eluard@gmail.com>
2025-05-03 17:18:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -79,4 +79,4 @@ def health():
 
 if __name__ == "__main__":
     logger.info("Lancement serveur...")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
